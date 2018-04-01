fb278473f7fd9035f56ed2af4c0f4e1aeb0d58ce
Daniel Wieczorek <daniel.wieczorek0@googlemail.com>
2018-04-01 21:38:59 +0200

    RSS-17::added reader

---
+++ rss-coinnewsasia/src/main/java/de/wieczorek/rss/core/RssReaderConfigurationProvider.java
@@ -0,0 +1,22 @@
+package de.wieczorek.rss.core;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class RssReaderConfigurationProvider implements RssConfig {
+
+    @Produces
+    @FeedUrl
+    private String feedUrl = "http://www.coinnewsasia.com/feed/";
+
+    @Produces
+    @RestPort
+    private int restPort = 8050;
+
+    @Produces
+    private MessageFilter filter = new NoOperationMessageFilter();
+
+    @Produces
+    private MessageTransformer transformer = new CoinnewsasiaMessageTransformer();
+}
