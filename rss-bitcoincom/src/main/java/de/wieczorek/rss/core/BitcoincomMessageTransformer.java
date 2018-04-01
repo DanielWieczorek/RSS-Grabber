60706283783cd2f0a48545b18d670f43dfc363fc
Daniel Wieczorek <daniel.wieczorek0@googlemail.com>
2018-04-01 21:40:02 +0200

    RSS-11::added reader

---
+++ rss-bitcoincom/src/main/java/de/wieczorek/rss/core/BitcoincomMessageTransformer.java
@@ -0,0 +1,28 @@
+package de.wieczorek.rss.core;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BitcoincomMessageTransformer implements MessageTransformer {
+
+    @Override
+    public RssEntry apply(RssEntry t) {
+	String description = t.getDescription().replaceAll("\n", "");
+	try {
+
+	    Pattern pattern = Pattern.compile(Pattern.quote("/>") + "(.*?)" + Pattern.quote("[&#8230;]"));
+
+	    Matcher matcher = pattern.matcher(description);
+	    matcher.find();
+	    description = matcher.group(1);
+
+	    t.setDescription(description);
+	    System.out.println(description);
+	} catch (Exception e) {
+	    System.out.println(description);
+	    e.printStackTrace();
+	}
+	return t;
+    }
+
+}
---
+++ rss-bitcoincom/src/main/java/de/wieczorek/rss/core/RssReaderConfigurationProvider.java
@@ -0,0 +1,22 @@
+package de.wieczorek.rss.core;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class RssReaderConfigurationProvider implements RssConfig {
+
+    @Produces
+    @FeedUrl
+    private String feedUrl = "https://news.bitcoin.com/feed/";
+
+    @Produces
+    @RestPort
+    private int restPort = 8080;
+
+    @Produces
+    private MessageFilter filter = new NoOperationMessageFilter();
+
+    @Produces
+    private MessageTransformer transformer = new BitcoincomMessageTransformer();
+}
