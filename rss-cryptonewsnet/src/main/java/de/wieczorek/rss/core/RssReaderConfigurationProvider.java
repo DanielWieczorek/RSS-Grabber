0c044a846c585768247e6eed4209670556c4f413
Daniel Wieczorek <daniel.wieczorek0@googlemail.com>
2018-04-01 21:33:31 +0200

    RSS-14::added reader for crypto-news.net

---
+++ rss-cryptonewsnet/src/main/java/de/wieczorek/rss/core/RssReaderConfigurationProvider.java
@@ -0,0 +1,22 @@
+package de.wieczorek.rss.core;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class RssReaderConfigurationProvider implements RssConfig {
+
+    @Produces
+    @FeedUrl
+    private String feedUrl = "https://www.crypto-news.net/feed/";
+
+    @Produces
+    @RestPort
+    private int restPort = 8030;
+
+    @Produces
+    private MessageFilter filter = new NoOperationMessageFilter();
+
+    @Produces
+    private MessageTransformer transformer = new NoOperationMessageTransformer();
+}
