b3d1769b08c0f921bc037c5bc4b52d7a3c159dcc
Daniel Wieczorek <daniel.wieczorek0@googlemail.com>
2019-01-05 22:51:47 +0100

    RSS-43::removed dependencies which were not needed

--- microservice-presentation/src/app/app.module.ts
+++ microservice-presentation/src/app/app.module.ts
@@ -2,17 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { MatButtonModule, MatButtonToggleModule, MatTabsModule} from '@angular/material'
-import { NgDragDropModule } from 'ng-drag-drop';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule,MatExpansionModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RssClassificationComponent } from './rss-classification/rss-classification.component';
 import { RssInsightComponent } from './rss-insight/rss-insight.component';
 import { MicroserviceStatusComponent } from './microservice-status/microservice-status.component';
 import { TradingSimulationComponent } from './trading-simulation/trading-simulation.component';
 import { RouterModule, Routes } from '@angular/router';
-import { ChartsModule } from 'ng2-charts';
 import { IntroductionComponent } from './introduction/introduction.component';
 
 const appRoutes: Routes = [
@@ -20,7 +14,7 @@ const appRoutes: Routes = [
                            { path: 'microservice-status', component: MicroserviceStatusComponent },
                            { path: 'rss-classification', component: RssClassificationComponent },
                            { path: 'rss-insight', component: RssInsightComponent},
-                           { path: 'trading-simulation', component: TradingSimulationComponent}
+                           { path: 'trading-simulation', component: TradingSimulationComponent},
                            { path: 'introduction', component: IntroductionComponent}
                            ];
 
@@ -35,18 +29,8 @@ const appRoutes: Routes = [
     IntroductionComponent
   ],
   imports: [
-    ChartsModule,
     BrowserModule,
-   
     HttpClientModule,
-    MatCardModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatListModule,
-    MatExpansionModule,
-    MatTabsModule,
-    BrowserAnimationsModule,
-    NgDragDropModule.forRoot(),
         RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
