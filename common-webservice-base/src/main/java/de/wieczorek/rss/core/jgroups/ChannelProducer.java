03ca09485b2d7ef5ca37aa4748fc14d0a911ca87
Daniel Wieczorek <daniel.wieczorek0@googlemail.com>
2019-03-17 11:40:48 +0100

    RSS-72::changed jgroups configuration such that the DB is used

--- common-webservice-base/src/main/java/de/wieczorek/rss/core/jgroups/ChannelProducer.java
+++ common-webservice-base/src/main/java/de/wieczorek/rss/core/jgroups/ChannelProducer.java
@@ -12,9 +12,9 @@ import org.jgroups.protocols.BARRIER;
 import org.jgroups.protocols.FD_ALL;
 import org.jgroups.protocols.FD_SOCK;
 import org.jgroups.protocols.FRAG2;
+import org.jgroups.protocols.JDBC_PING;
 import org.jgroups.protocols.MERGE3;
 import org.jgroups.protocols.MFC;
-import org.jgroups.protocols.MPING;
 import org.jgroups.protocols.TCP;
 import org.jgroups.protocols.UNICAST3;
 import org.jgroups.protocols.VERIFY_SUSPECT;
@@ -34,10 +34,17 @@ public class ChannelProducer {
     @Produces
     private JChannel buildInfoChannel() throws UnknownHostException, Exception {
 	return new JChannel(new TCP() //
-		.setValue("bind_addr", Inet4Address.getLoopbackAddress()) //
+		.setValue("bind_addr", Inet4Address.getLocalHost()) //
 		.setValue("bind_port", bindPort) //
-		, new MPING() //
-		, new MERGE3() //
+		, new JDBC_PING().setValue("connection_driver", "org.postgresql.Driver")//
+			.setValue("connection_password", "admin")//
+			.setValue("connection_url", "jdbc:postgresql://localhost/MICROSERVICE_COMMON")//
+			.setValue("connection_username", "postgres")//
+			.setValue("initialize_sql",
+				"CREATE TABLE JGROUPSPING (" + "own_addr varchar(200) NOT NULL, "
+					+ "cluster_name varchar(200) NOT NULL, " + "ping_data bytea DEFAULT NULL, "
+					+ "PRIMARY KEY (own_addr, cluster_name) )"),
+		new MERGE3() //
 		, new FD_SOCK() //
 		, new FD_ALL() //
 			.setValue("timeout", 12000) //
