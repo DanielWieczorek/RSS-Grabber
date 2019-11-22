ac38986bfdf6901e108929a67311ccffec5e499e
Daniel Wieczorek <daniel.wieczorek0@googlemail.com>
2019-11-22 14:08:13 +0100

    RSS-117::adapted writing config with optionals

--- trading-common/src/main/java/de/wieczorek/rss/trading/common/oracle/OracleConfigurationDao.java
+++ trading-common/src/main/java/de/wieczorek/rss/trading/common/oracle/OracleConfigurationDao.java
@@ -1,6 +1,7 @@
 package de.wieczorek.rss.trading.common.oracle;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import de.wieczorek.nn.NeuralNetworkName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,29 +22,31 @@ public class OracleConfigurationDao {
 
     public OracleConfiguration read() {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Jdk8Module());
         File file = new File(buildPath());
 
         try {
-            return mapper.readValue(file,OracleConfiguration.class);
+            return mapper.readValue(file, OracleConfiguration.class);
         } catch (IOException e) {
-            logger.error("Could not read file "+file.getAbsolutePath(),e);
+            logger.error("Could not read file " + file.getAbsolutePath(), e);
             return null;
         }
     }
 
     public void write(OracleConfiguration configuration) {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Jdk8Module());
         File file = new File(buildPath());
         try {
             file.createNewFile();
-            mapper.writeValue(file,configuration);
+            mapper.writeValue(file, configuration);
         } catch (IOException e) {
-            logger.error("Could not read write value to file "+file.getAbsolutePath(),e);
+            logger.error("Could not read write value to file " + file.getAbsolutePath(), e);
         }
     }
 
 
-    private String buildPath(){
+    private String buildPath() {
         return Paths.get(System.getProperty("user.home"), "neural-networks", fileName).toString();
     }
 }
