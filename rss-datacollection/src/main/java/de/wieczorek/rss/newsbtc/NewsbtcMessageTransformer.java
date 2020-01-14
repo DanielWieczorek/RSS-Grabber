92ca77b2d6efc93c650feb31ac74115a06e6fecc
Daniel Wieczorek <daniel.wieczorek0@googlemail.com>
2020-01-14 21:21:05 +0100

    RSS-136::fix parsing error

--- rss-datacollection/src/main/java/de/wieczorek/rss/newsbtc/NewsbtcMessageTransformer.java
+++ rss-datacollection/src/main/java/de/wieczorek/rss/newsbtc/NewsbtcMessageTransformer.java
@@ -5,22 +5,13 @@ import de.wieczorek.rss.types.RssEntry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class NewsbtcMessageTransformer implements MessageTransformer {
     private static final Logger logger = LoggerFactory.getLogger(NewsbtcMessageTransformer.class);
 
     @Override
     public RssEntry apply(RssEntry t) {
-        String description = t.getDescription().replaceAll("\n", "");
+        String description = t.getDescription().replaceAll("\n", "").replace("... The post appeared first on NewsBTC.", "");
         try {
-
-            Pattern pattern = Pattern.compile(Pattern.quote("<p>") + "(.*?)" + Pattern.quote("<a"));
-
-            Matcher matcher = pattern.matcher(description);
-            matcher.find();
-            description = matcher.group(1);
 
             t.setDescription(description);
             logger.info("extracted: " + description);
